86cd0b3b92c7af02180589bf02dec5a26965cf8f
Aaron Santana <84732450+Santaval@users.noreply.github.com>
2022-12-01 23:34:34 -0600

    number theory

--- Python/number_teory.py
+++ Python/number_teory.py
@@ -3,45 +3,46 @@ def main():
   selection = str.upper(input("\nChoose an option: \n P: Prime number\n C: Compose number\n R: Relative primes\n D: Divisor\n"))
   
   if selection == "P":
-    print(str(isPrime()))
+    number = setNumber("Digit a number")
+    print(str(isPrime(number)))
   elif selection == "C":
     print(str(not isPrime()))
   elif selection == "R":
-    print(str(areRelativesPrimes()))
+    number1 = setNumber("Digit first number")
+    number2 = setNumber("Digit second number")
+    print(str(areRelativesPrimes(number1, number2)))
   elif selection == "D":
-    print(str(isDivisor()))   
+    dividing = setNumber("Dividing number")
+    divisor = setNumber("Divisor number")
+    print(str(isDivisor(dividing, divisor)))   
 
 
-def isPrime():
-  number = setNumber("Digit a number")
+def isPrime(number):
   for counter  in range(number):
-      if counter > 1 and number % counter == 0: 
+      if counter > 1 and isDivisor(number, counter): 
        return False
   return True 
 
-def areRelativesPrimes():
-  number1 = setNumber("Digit first number")
-  number2 = setNumber("Digit second number")
-  number1Divisors = []
-  number2Divisors = []
-  for counter in range(number1):
-    if counter > 1 and number1 % counter == 0:
-      number1Divisors.append(counter)
-  for counter in range(number2):
-    if counter > 1 and number2 % counter == 0:
-      number2Divisors.append(counter)  
+def areRelativesPrimes(number1, number2):
+  number1Divisors = setDivisors(number1)
+  number2Divisors = setDivisors(number2)
 
   for num in number1Divisors:
     if num in number2Divisors:
       return False
   return True          
 
-def isDivisor():
-  dividing = setNumber("Dividing number")
-  divisor = setNumber("Divisor number")
+def isDivisor(dividing, divisor):
   if (dividing % divisor == 0):
     return True
   return False 
+
+def setDivisors(number):
+  divisors = []
+  for counter in range(number):
+    if counter > 1 and  isDivisor(number, counter):
+      divisors.append(counter)
+  return divisors
 
 def setNumber(text): 
   return  int(input(text+":\n")) 
